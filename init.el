84c770f761d2254aaf0c535317ba071b9f9ca604
Eddie Maldonado <eddie@lemald.org>
2022-12-02 19:59:38 -0500

    feat: tide

--- init.el
+++ init.el
@@ -130,9 +130,33 @@
       (concat (file-name-as-directory (getenv "HOME"))
 	      (file-name-as-directory "elixir_ls"))))
 
+(use-package prettier-js)
+
+(use-package add-node-modules-path)
+
+(use-package tide
+  :init
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    (company-mode +1)
+    (add-node-modules-path)
+    (prettier-js-mode))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  (add-hook 'web-mode-hook (lambda ()
+			     (when (string-equal
+				     "tsx"
+				     (file-name-extension buffer-file-name))
+			       (setup-tide-mode)))))
+
 (use-package web-mode
   :mode
-  "\\.html\\.eex\\'")
+  "\\.html\\.eex\\'"
+  "\\.tsx\\'")
 
 (use-package markdown-mode
   :config
